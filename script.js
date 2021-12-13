430ddd297eba009661fc0e564c14f2f5f7420891
Aymane Ibrahim Zaitoune <80549630+Aigle06@users.noreply.github.com>
2021-12-14 00:07:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ document.querySelector('#cart-btn').onclick = () =>{
 }
 
 window.onscroll = () =>{
-    navbar.classList.remove('active');
-    searchForm.classList.remove('active');
-    cartItem.classList.remove('active');
+     navbar.classList.remove('active');
+     searchForm.classList.remove('active');
+     cartItem.classList.remove('active');
 }
